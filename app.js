3b7cf64bc5f56af2d971359f1762752f4accf92f
Navruz <navuz.ochilov98@mail.ru>
2022-05-11 14:43:43 +0300

    create mern app structure

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+
+const app = express();
+
+const PORT = config.get("port") || 5000;
+
+app.use("/api/auth", require("./routes/auth.routes.js"));
+
+async function start() {
+  try {
+    await mongoose.connect(config.get("mongoUri"), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+}
+
+start();
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
